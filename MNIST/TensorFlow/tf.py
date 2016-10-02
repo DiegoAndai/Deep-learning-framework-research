6dc6877d1815b0a5d0554ff04bd7766f12b827f9
Vicente Valencia <vmvalencia@uc.cl>
2016-10-02 00:29:25 -0300

    Added average precision and recall for tensorflow mnist implementation and added precision and recall for each class and average for torch mnist implementation.

--- MNIST/TensorFlow/tf.py
+++ MNIST/TensorFlow/tf.py
@@ -43,24 +43,25 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 # Define loss and optimizer
 y_ = tf.placeholder(tf.float32, [None, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
 
 tf.initialize_all_variables().run()
 
 # Train
 for i in range(1000):
-  batch_xs, batch_ys = mnist.train.next_batch(100)
-  train_step.run({x: batch_xs, y_: batch_ys})
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    train_step.run({x: batch_xs, y_: batch_ys})
 
 
 # Test trained model
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
 
 
 predicted_classes = tf.argmax(y, 1)  # 1-D tensor with classes predicted for each image.
 true_classes = tf.argmax(y_, 1)  # 1-D tensor with the actual classes.
+
+avg_precision = avg_recall = 0
 for cls in range(10):
 
     cls_is_predicted = tf.equal(predicted_classes, cls)  # True when cls was predicted, false otherwise.
@@ -75,6 +76,16 @@ for cls in range(10):
 
     precision = tf.div(tp, tf.add(fp, tp))
     recall = tf.div(tp, tf.add(fn, tp))
-    print("Class {}:\nPrecision: {}\nRecall: {}\n".format(cls,
-                                                          precision.eval({x: mnist.test.images, y_: mnist.test.labels}),
-                                                          recall.eval({x: mnist.test.images, y_: mnist.test.labels})))
+
+    current_p = precision.eval({x: mnist.test.images, y_: mnist.test.labels})  # evaluate precision
+    current_r = recall.eval({x: mnist.test.images, y_: mnist.test.labels})  # evaluate recall
+
+    avg_precision += current_p
+    avg_recall += current_r
+
+    print("Class {}:\nPrecision: {}\nRecall: {}\n".format(cls, current_p, current_r))
+
+avg_precision /= 10
+avg_recall /= 10
+print("Average precision: {}\nAverage recall: {}".format(avg_precision, avg_recall))
+print("Accuracy: {}\n".format(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels})))
--- MNIST/Torch/torch.lua
+++ MNIST/Torch/torch.lua
@@ -29,13 +29,55 @@ trainer:train(trainset)
 --testing
 correct = 0
 for i=1,10000 do
-    local groundtruth = testset.label[i]
-    local prediction = net:forward(testset.data[i])
-    local confidences, indices = torch.sort(prediction, true)  -- true means sort in descending order
+    groundtruth = testset.label[i]  -- actual digit
+    prediction = net:forward(testset.data[i])  -- predicted digit (log probabilities)
+    confidences, indices = torch.sort(prediction, true)  -- true means sort in descending order
     if groundtruth == indices[1] then
         correct = correct + 1
     end
 end
 
-print(correct, 100*correct/10000 .. ' % ') -- accuracy
---]]
+print('Hits: ' .. correct, 'Accuracy: ' .. 100*correct/10000 .. ' % ') -- accuracy
+
+
+-- precision and recall
+
+avg_recall, avg_precision = 0, 0
+for cls=1,#classes do
+	tp, fn, fp = 0, 0 ,0
+	-- code repetition (omg!)
+	for i=1,10000 do
+		groundtruth = testset.label[i]  -- actual digit
+		prediction = net:forward(testset.data[i])  -- predicted digit (log probabilities)
+		confidences, indices = torch.sort(prediction, true)  -- true means sort in descending order
+
+
+		if groundtruth == cls then  -- if the actual digit is of the class cls
+		    
+		    if groundtruth == indices[1] then  -- if prediction was correct
+		        tp = tp + 1
+		    else  -- if prediction was incorrect
+		    	fn = fn + 1
+		    end
+
+		else  -- if the actual digit is not of class cls
+
+			if indices[1] == cls then -- and class cls was predicted
+				fp = fp + 1
+			end
+
+		end
+	end
+	precision = tp/(tp + fp)
+	recall = tp/(tp + fn)
+	avg_precision = avg_precision + precision
+	avg_recall = avg_recall + recall
+
+	print("Class " .. cls - 1 .. ":\nPrecision: " .. precision .. "\nRecall: " .. recall)
+end
+
+avg_recall = avg_recall/#classes
+avg_precision = avg_precision/#classes
+print("Average precision: " .. avg_precision)
+print("Average recall: " .. avg_recall)
+
