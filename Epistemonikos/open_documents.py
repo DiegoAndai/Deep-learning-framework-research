2583a38c140f742b1f79e13f3c60f1f19a0d9cdb
Vicente Valencia <vmvalencia@uc.cl>
2016-11-23 15:52:41 -0300

    First implementation

--- Epistemonikos/open_documents.py
+++ Epistemonikos/open_documents.py
@@ -1,30 +1,31 @@
 import string
 
-class SentenceReader:
 
-    def __init__(self, files):
-        self.files = files
+class PaperReader:
+
+    def __init__(self, papers):
+        self.papers = papers
         self.keep = string.ascii_lowercase + '-'
         self.dismiss = "123456789"
+        self.words = []
 
     def __iter__(self):
-        for file in self.files:
+        for paper in self.papers:
             try:
-                abstract = file["abstract"]
+                abstract = paper["abstract"]
                 if abstract:
                     yield self.parse_line(abstract)
 
             except KeyError:
-                print("no abstract for file {}".format(file["id"]))
+                print("no abstract for paper {}".format(paper["id"]))
         raise StopIteration
 
     def __getitem__(self, index):
-        abstract = self.files[index]["abstract"]
+        abstract = self.papers[index]["abstract"]
         if abstract:
             return self.parse_line(abstract)
         else:
             return None
-
 
     def parse_line(self, line):
             line = line.lower()
@@ -37,3 +38,18 @@ class SentenceReader:
             if ch in word:
                 return None
         return "".join(ch for ch in word if ch in self.keep)
+
+    def generate_words_list(self):
+        i = 0
+        for abstract in self:
+            if abstract:
+                self.words += abstract
+            if i % 50000 == 0:
+                print("{} papers parsed so far".format(i))
+            i += 1
+        print("Total word count:", len(self.words))
+
+    def save_words(self):
+        with open("words.txt", 'w') as words_file:
+            for w in self.words:
+                words_file.write("{}\n".format(w))
