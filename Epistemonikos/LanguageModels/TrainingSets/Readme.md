6be4be39230a02248a366f9b0f51ae9f0c3a25e7
Vince-Valence <vmvalencia@uc.cl>
2017-01-08 18:01:28 -0300

    Fixed papers balancing

--- Epistemonikos/LanguageModels/TrainingSets/Readme.md
+++ Epistemonikos/LanguageModels/TrainingSets/Readme.md
@@ -1,7 +1,12 @@
 Here, the following training sets in the form of common text files can 
 be found:
 
-* episte_sys_prima: text file with only primary studies and systematic 
+* episte_sys_prima1: text file with only primary studies and systematic 
 reviews of the first 80% of the available Epistemonikos papers (52MB).
+Potentially wrong: may have accidentally considered some, and not all,
+the words of each abstract. Avoid training with this file.
+* episte_sys_prima2: text file with only primary studies and
+systematic reviews belonging to the first 80% of available data (248705 
+papers). Each abstract used had at least 30 words (size: 354 MB).
 * allwiki: text from Wikipedia (523MB).
 * text8: unkown properties. 
--- Epistemonikos/PaperProcessing/open_documents.py
+++ Epistemonikos/PaperProcessing/open_documents.py
@@ -30,20 +30,31 @@ class PaperReader:
         self.filter_by = filters if filters else []  # filter by paper classification (systematic-review,
         # structured-summary-of-systematic-review, primary-study, overview, structured-summary-of-primary-study)
 
+        print("filtering and parsing abstracts...")
         self.filtered_papers = list(map(lambda pap: {"abstract": ' '.join(self.parse_line(pap["abstract"])),
                                                      "classification": pap["classification"], "id": pap["id"]},
                                         [p for p in filter(lambda paper: paper["classification"] in self.filter_by,
                                                            self._papers)] if self.filter_by else self._papers))
+        print("finished filtering and parsing.\n"
+              "Papers before filtering: {}\n"
+              "Papers after filtering: {}\n".format(len(self._papers), len(self.filtered_papers)))
 
+        print("splitting papers...")
         divide = int(len(self.filtered_papers) * train_percent / 100)
         self.filtered_train_papers = self.filtered_papers[:divide]
         self.filtered_test_papers = self.filtered_papers[divide:] if divide < len(self._papers) else []
+        print("papers split. Train papers: {}\nTest papers: {}\n".format(len(self.filtered_train_papers),
+                                                                         len(self.filtered_test_papers)))
 
         if even_train:
+            print("balancing train papers...")
             self.filtered_train_papers = self.even_papers(self.filtered_train_papers)
+            print("train papers balanced. Train papers after balance: {}".format(len(self.filtered_train_papers)))
 
         if even_test:
+            print("balancing test papers...")
             self.filtered_test_papers = self.even_papers(self.filtered_test_papers)
+            print("test papers balanced. Test papers after balance: {}".format(len(self.filtered_test_papers)))
 
         self.loop_train = True  # whether __iter__ should loop over train or test papers.
 
@@ -78,7 +89,7 @@ class PaperReader:
                 try:
                     abstract = paper["abstract"]
                     if abstract:
-                        yield self.parse_line(abstract)
+                        yield abstract.split(' ')
                     else:
                         print("Abstract empty for paper {}".format(paper["id"]))
                         yield []
@@ -176,6 +187,7 @@ class PaperReader:
         for p in papers:
             appearances[p["classification"]] += 1
         l_f = min(appearances, key=lambda x: appearances[x])
+        print(appearances)
         return l_f, appearances[l_f]
 
     @staticmethod
@@ -184,6 +196,7 @@ class PaperReader:
         """Balances the papers, eliminating class imbalance."""
 
         classes = list(set(p["classification"] for p in papers))
+        print(classes)
         lf, lfapp = PaperReader.get_less_freq_cls(papers, classes)
 
         new_appearances = dict.fromkeys(classes, 0)
@@ -192,8 +205,10 @@ class PaperReader:
             current_class_apps = new_appearances[p["classification"]]
             if current_class_apps < lfapp:
                 even_papers.append(p)
+                new_appearances[p["classification"]] += 1
             if min(new_appearances.values()) >= lfapp:
                 break
+        print(new_appearances)
         return even_papers
 
 
@@ -201,6 +216,4 @@ if __name__ == '__main__':
 
     with open("documents_array.json", encoding="utf-8") as docs:
         reader = PaperReader(json.load(docs), filters=["systematic-review", "primary-study"], train_percent=80,
-                             abstracts_min=30)
-    reader.generate_words_list()
-    reader.dump_text("episte_sys_prima")
+                             abstracts_min=30, even_test=True)
