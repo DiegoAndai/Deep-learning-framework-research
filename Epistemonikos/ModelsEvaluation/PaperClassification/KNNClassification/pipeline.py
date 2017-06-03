bf168eff93f1403c947bd7870805ab343b2505d8
DiegoAndai <dfandai@uc.cl>
2017-06-02 22:06:20 -0400

    pipeline

--- Epistemonikos/ModelsEvaluation/PaperClassification/KNNClassification/pipeline.py
+++ Epistemonikos/ModelsEvaluation/PaperClassification/KNNClassification/pipeline.py
@@ -8,8 +8,11 @@ for k in [100]:
     for i in range(5):
         acc, conf, restricted_dict = classify.main(k, i, restrict_random = True)
         conf_list = [list(conf[0]), list(conf[1])]
-        results[k].append([acc, conf_list, k, restricted_dict])
+        results[k].append([acc, conf_list, k])
     with open("results_denis_new_round_{}k.json".format(k), "w") as json_out:
         json.dump(results, json_out)
 
     print("test with k: {} completed".format(k))
+
+'''python pipeline.py --K 10 --model_path ../../../LanguageModels/Skipgram/Advanced/TrainedModels/Model4 --span 80 --KNN_papers_set Set4
+'''
